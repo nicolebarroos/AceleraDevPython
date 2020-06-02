f0f2d7e87761039411ac0d2690e2f1b39b8480dd
Nicole <barrosnicole094@gmail.com>
2020-06-02 15:56:46 -0300

    Desafio 8

---
+++ python-8/main.py
@@ -0,0 +1,24 @@
+import jwt
+
+#ref: https://pyjwt.readthedocs.io/en/latest/
+
+secret = 'acelera'
+
+def create_token(data, secret):
+    encoded_jwt = jwt.encode(data, secret, algorithm='HS256')
+        
+    return encoded_jwt
+
+
+   
+
+def verify_signature(token):
+    try:
+        decoded_jwt = jwt.decode(token, secret, algorithms=['HS256'])
+
+    except:
+        decoded_jwt = {'error': 2}
+
+    return decoded_jwt
+
+
